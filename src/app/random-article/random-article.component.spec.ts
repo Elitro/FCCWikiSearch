778c75970c78d0278205f04a2c168167560ba3e8
Elitro <Elitro>
2016-10-10 23:02:27 +0100

    init

---
+++ src/app/random-article/random-article.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { RandomArticleComponent } from './random-article.component';
+
+describe('Component: RandomArticle', () => {
+  it('should create an instance', () => {
+    let component = new RandomArticleComponent();
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/random-article/random-article.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-random-article',
+  templateUrl: './random-article.component.html',
+  styleUrls: ['./random-article.component.css']
+})
+export class RandomArticleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
